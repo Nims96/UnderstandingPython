be0b06b5b144df125b08135845b0266112827574
Malicarus <tylerarcher96@gmail.com>
2020-07-08 18:27:27 -0500

    added show board and hand options to the menu

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ print("\n" + b + c)
 
 print("Player 1 Begin")
 
-print("\n1 Deployment \n2 Combat \n3 Deployment 2 \n4 End Turn")
+print("\n1: Deployment \n2: Combat \n3: Deployment 2 \n4: End Turn \n5: Show board \n6: Show hand")
 
 player_choice= int(input("Pick the phase you would like to go to "))
 
@@ -14,8 +14,21 @@ combat_passed = False
 deployment2_passed = False
 end_turn = False
 
+player1_hand = ["Rathalos", "Ruby", "Emerald"]
 
-while player_choice <= 4 and player_choice >= 1:
+player1_creature_zone = ["Rathian"]
+player1_resource_zone = ["Ruby", "Ruby", "Ruby", "Ruby", "Emerald", "Emerald", "Emerald"]
+player1_used_resources = ["Ruby"]
+player1_unused_resources = ["Ruby", "Ruby", "Ruby", "Emerald", "Emerald", "Emerald"]
+
+player2_hand = ["Frozen Bite", "Hurl Snow-Boulder", "Barioth"]
+
+player2_creature_zone = ["Barioth"]
+player2_resource_zone = ["Sapphire", "Sapphire", "Sapphire", "Sapphire", "Sapphire"]
+player2_used_resources = ["Sapphire", "Sapphire", "Sapphire"]
+player2_unused_resources = ["Sapphire", "Sapphire"]
+
+while player_choice <= 6 and player_choice >= 1:
     if player_choice == 1 and deployment1_passed == False:
         print("You are now in Deployment 1")
         """Deployment stuff"""
@@ -39,6 +52,23 @@ while player_choice <= 4 and player_choice >= 1:
         deployment1_passed = False
         combat_passed = False
         break
+    elif player_choice == 5:
+        print("Here is the board")
+        print("Your creatures: " + str(player1_creature_zone))
+        print("Your total resources are: " + str(player1_resource_zone))
+        print("Unused resources: " + str(player1_unused_resources))
+        print("Your used resources are: " + str(player1_used_resources))
+        
+        print("\n\nHere is you opponents board")
+        print("Your opponent has " + str(len(player2_hand)) + " cards in hand")
+        print("Your opponents creatures: " + str(player2_creature_zone))
+        print("Your opponents total resources are: " + str(player2_resource_zone))
+        print("Your opponents unused resources: " + str(player2_unused_resources))
+        print("Your opponents used resources are: " + str(player2_used_resources))
+
+    elif player_choice == 6:
+        print("Here is/are the card/cards in your hand")
+        print(player1_hand)
     else:
         if deployment1_passed == False and combat_passed == False and deployment2_passed == False and end_turn == False:
             print("Invalid Selection, please select one of the following options \n1 Deployment \n2 Combat \n3 Deployment 2 \n4 End Turn")
