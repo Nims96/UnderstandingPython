8e74deebecf62687e80db78e0f4cd1346700da81
Malicarus <tylerarcher96@gmail.com>
2020-07-10 17:26:52 -0500

    setting up for combat inner steps + made show_board function

--- Main.py
+++ Main.py
@@ -7,10 +7,6 @@ print("\n" + b + c)
 
 print("Player 1 Begin")
 
-deployment1_passed = False
-combat_passed = False
-deployment2_passed = False
-
 player1_hand = ["Rathalos", "Ruby", "Emerald"]
 player1_creature_zone = ["Rathian"]
 player1_resource_zone = ["Ruby", "Ruby", "Ruby", "Ruby", "Emerald", "Emerald", "Emerald"]
@@ -22,6 +18,25 @@ player2_creature_zone = ["Barioth"]
 player2_resource_zone = ["Sapphire", "Sapphire", "Sapphire", "Sapphire", "Sapphire"]
 player2_used_resources = ["Sapphire", "Sapphire", "Sapphire"]
 player2_unused_resources = ["Sapphire", "Sapphire"]
+
+
+def show_board():
+    print("\nHere is the board")
+    print("Your creatures: " + str(player1_creature_zone))
+    print("Your total resources are: " + str(player1_resource_zone))
+    print("Unused resources: " + str(player1_unused_resources))
+    print("Your used resources are: " + str(player1_used_resources))
+
+    print("\n\nHere is you opponents board")
+    print("Your opponent has " +
+          str(len(player2_hand)) + " cards in hand")
+    print("Your opponents creatures: " + str(player2_creature_zone))
+    print("Your opponents total resources are: " +
+          str(player2_resource_zone))
+    print("Your opponents unused resources: " +
+          str(player2_unused_resources))
+    print("Your opponents used resources are: " +
+          str(player2_used_resources))
 
 def inner_options(phase_number):
     if phase_number == 1:
@@ -36,7 +51,7 @@ def inner_options(phase_number):
         
         inner_phase_choice = int(input("Pick the phase you would like to go to "))
 
-        while inner_phase_choice != 4:
+        while True:
             if inner_phase_choice == 1:
                 print("Select a creature from your hand that you would like to play")
             elif inner_phase_choice == 2:
@@ -44,10 +59,9 @@ def inner_options(phase_number):
             elif inner_phase_choice == 3:
                 print("Select a spell from your hand that you would like to play")
             elif inner_phase_choice == 4:
-                deployment1_passed = True
-                phase_number = 2
+                break
             elif inner_phase_choice == 5:
-                print("Here is/are the card/cards in your hand")
+                print("Here is your hand")
                 print(player1_hand)
             else:
                 print("That is not a valid choice. Please select one of the following:")
@@ -57,31 +71,25 @@ def inner_options(phase_number):
                 print("4: Proceed to Next Phase")
                 print("5: Show Hand")
                 inner_phase_choice = int(input("Select one of the above actions to take"))
+    elif phase_number == 2:
+        print("Here are your creatures: " + str(player1_creature_zone))
+        print("Here are your opponents creatures: " + str(player2_creature_zone))
+
+        print("1: Select creatures you want to attack with")
+        print("2: Skip attacking")
+        print("3: Show boards")
+
+        
 
 
 
 def loop_player():
     print("\n1 Deployment \n2 Combat \n3 Deployment 2 \n4 End Turn \n5 Show Board \n6 Show Hand")
-    phase_choice = input("Pick the phase you would like to go to ")
+    phase_choice = int(input("Pick the phase you would like to go to "))
     
     deployment1_passed = False
     combat_passed = False
     deployment2_passed = False
-
-    test = 0
-
-    while test == 0:
-        if phase_choice.isalpha() == True:
-            print("You must select a number in order to pick a phase")
-            phase_choice = input("Pick the phase you would like to go to ")
-        elif phase_choice.isdigit() == True:
-            phase_choice = int(phase_choice)
-            test = 1
-        else:
-            print("You have selected nothing, Please make a choice:")
-            print("\n1 Deployment \n2 Combat \n3 Deployment 2 \n4 End Turn \n5 Show Board \n6 Show Hand")
-            phase_choice = input("Please select one of the above options: ")
-
 
     while phase_choice > 6 or phase_choice < 1:
         print("Invalid Selection, please select one of the following options \n1 Deployment \n2 Combat \n3 Deployment 2 \n4 End Turn \n5 Show Board \n6 Show Hand")
@@ -111,19 +119,7 @@ def loop_player():
             combat_passed = False
             break
         elif phase_choice == 5:
-            print("Here is the board")
-            print("Your creatures: " + str(player1_creature_zone))
-            print("Your total resources are: " + str(player1_resource_zone))
-            print("Unused resources: " + str(player1_unused_resources))
-            print("Your used resources are: " + str(player1_used_resources))
-        
-            print("\n\nHere is you opponents board")
-            print("Your opponent has " + str(len(player2_hand)) + " cards in hand")
-            print("Your opponents creatures: " + str(player2_creature_zone))
-            print("Your opponents total resources are: " + str(player2_resource_zone))
-            print("Your opponents unused resources: " + str(player2_unused_resources))
-            print("Your opponents used resources are: " + str(player2_used_resources))
-
+            show_board()
         elif phase_choice == 6:
             print("Here is/are the card/cards in your hand")
             print(player1_hand)
