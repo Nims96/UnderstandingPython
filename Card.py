14d951aa0abad503433dd6409819ba399bbfa9a1
nimsc <nimscurtis@gmail.com>
2020-07-17 18:13:29 -0500

    removal of space in description

--- Card.py
+++ Card.py
@@ -2,11 +2,4 @@ class card_class:
     def __init__(self,name,faction,description):
         self.name = name
         self.faction = faction
-        self. description = description
-
-
-
-
-
-
-
+        self.description = description
