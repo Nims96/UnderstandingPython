e45fec5be15dcdb93603179dfed77f19a4199770
Malicarus <tylerarcher96@gmail.com>
2020-07-17 09:04:08 -0500

    removed description attribute since it is unnecessary for now

--- Card.py
+++ Card.py
@@ -1,24 +1,23 @@
 class Card_class:
-    def __init__(self, name, faction, description):
+    def __init__(self, name, faction):
         self.name = name
         self.faction = faction
-        self.description = description
 
 class Creature(Card_class):
-    def __init__(self, name, faction, description, attack, hp, cost, is_tapped):
-        super().__init__(name, faction, description)
+    def __init__(self, name, faction, attack, hp, cost, is_tapped):
+        super().__init__(name, faction)
         self.attack     = attack
         self.hp         = hp
         self.cost       = cost
         self.is_tapped  = is_tapped
 
 class Spell(Card_class):
-    def __init__(self, name, faction, description, cost, effect):
-        super().__init__(name, faction, description)
+    def __init__(self, name, faction, cost, effect):
+        super().__init__(name, faction)
         self.cost = cost
         self.effect = effect
 
 class Resource(Card_class):
-    def __init__(self, name, faction, description, is_tapped):
-        super().__init__(name, faction, description)
+    def __init__(self, name, faction, is_tapped):
+        super().__init__(name, faction)
         self.is_tapped = is_tapped
